59797c39ae4946bd2299a4507d919574ae66244d
sdfwefk11 <mknhsad3398@naver.com>
2023-05-21 18:36:25 +0900

    dragConstraints

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
 `;
 const Box = styled(motion.div)`
   width: 200px;
@@ -21,15 +31,21 @@ const BoxVariants = {
   drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 };
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={BoxVariants}
-        whileHover="hover"
-        whileTap="click"
-        whileDrag="drag"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          dragSnapToOrigin
+          dragElastic={0.5}
+          variants={BoxVariants}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
