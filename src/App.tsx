a660bcd26e05789d64b315d5b325d5303748568a
sdfwefk11 <mknhsad3398@naver.com>
2023-05-28 19:50:54 +0900

    layoutId prop3

--- src/App.tsx
+++ src/App.tsx
@@ -34,26 +34,32 @@ const Overlay = styled(motion.div)`
   align-items: center;
 `;
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => {
-    setClicked((prev) => !prev);
-  };
+  const [id, setId] = useState<null | string>(null);
+  console.log(id);
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="1" />
-        <Box />
-        <Box />
-        <Box />
+        {["1", "2", "3", "4"].map((item) => (
+          <Box
+            onClick={() => {
+              setId(item);
+            }}
+            key={item}
+            layoutId={item}
+          />
+        ))}
       </Grid>
       <AnimatePresence>
-        {clicked ? (
+        {id ? (
           <Overlay
+            onClick={() => {
+              setId(null);
+            }}
             initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "rgba(0,0,0,1)" }}
+            animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
             exit={{ backgroundColor: "rgba(0,0,0,0)" }}
           >
-            <Box layoutId="1" style={{ width: 400, height: 200 }} />
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
           </Overlay>
         ) : null}
       </AnimatePresence>
